297430b04b59e568532489357294e88b521b352d
AmulyaKumar1 <toamulya@gmail.com>
2026-09-12 11:10:37 -0400

    first version

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Movies Store'
+    return render(request, 'home/index.html', {
+        'template_data': template_data})
+
+# Create your views here.
+
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 
+                  'home/about.html',
+                  {'template_data': template_data})
